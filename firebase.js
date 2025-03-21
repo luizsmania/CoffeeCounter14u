90bd3a37dbeddc5e317cde1f56cb233f97a0eb6e
Luiz Smania <66436972+luizsmania@users.noreply.github.com>
2025-03-21 08:12:42 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -88,4 +88,9 @@ export function exportData() {
     console.log('Exported data:', jsonData);
 }
 
-document.getElementById('added').addEventListener('click', exportData);
+
+function scheduleExportData() {
+}
+
+// Start the scheduling
+scheduleExportData();
